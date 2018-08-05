efecac5283dc3c509dcaa09a5629c2bd66d37512
Nissim Hania <nissim.hania@gmail.com>
2018-08-05 20:12:21 +0300

    socket client side

--- src/main.js
+++ src/main.js
@@ -11,8 +11,14 @@ import practice from '@/components/practice'
 import footerNextPage from '@/components/footer'
 import parent from '@/components/parent'
 import child from '@/components/child'
+import socketio from 'socket.io-client'
+import VueSocketIO from 'vue-socket.io'
 
 Vue.config.productionTip = false
+
+const socketURL = 'http://localhost:8050/'
+export const SocketInstance = socketio(socketURL)
+Vue.use(VueSocketIO, SocketInstance)
 
 Vue.use(Router)
 
